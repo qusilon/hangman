d1747ab25276b3b63a1a2ce8f9bf6f64fc38306d
qusilon <aleks98_06@mail.ru>
2026-03-09 20:56:17 +0700

    game menu added

--- src/main/java/org/qusilon/App.java
+++ src/main/java/org/qusilon/App.java
@@ -1,7 +1,31 @@
 package org.qusilon;
 
+import java.util.Scanner;
+
 public class App {
     static void main(String[] args) {
-        System.out.println("Hello, world!");
+        runMenu();
+    }
+
+    static void runMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Добро пожаловать в игру Виселица!");
+        while (true) {
+            System.out.println("Введите нужную цифру из меню и нажмите Enter.");
+            System.out.println("1 - Новая игра");
+            System.out.println("0 - Выход");
+            System.out.print("Ваш выбор: ");
+            String choice = scanner.next();
+            System.out.println();
+
+            if (choice.equals("1")) {
+                System.out.println("Тут будет игра");
+            } else if (choice.equals("0")) {
+                scanner.close();
+                break;
+            } else {
+                System.out.println("Такой цифры в меню нет, попробуте еще раз.");
+            }
+        }
     }
 }
